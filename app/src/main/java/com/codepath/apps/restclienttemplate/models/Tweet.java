68b300773f61fc34b01aa69ee7098dfd0ee2773c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-04-22 15:45:56 -0400

    Persistence in sql db

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,11 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,10 +16,23 @@ import java.util.List;
 import com.codepath.apps.restclienttemplate.TimeFormatter;
 
 @Parcel
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "userId"))
 public class Tweet {
-    public String body;
-    public String created_at;
+
+    @ColumnInfo
+    @PrimaryKey
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String created_at;
+
+    @ColumnInfo
+    public long userId;
+
+    @Ignore
     public User user;
 
     // empty constructor needed by the Parceler library
@@ -24,7 +43,9 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.created_at = jsonObject.getString("created_at");
         tweet.id = jsonObject.getLong("id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        User user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.user = user;
+        tweet.userId = user.id;
         return tweet;
     }
 
