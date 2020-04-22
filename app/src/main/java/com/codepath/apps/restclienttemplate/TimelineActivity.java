8c4f7054a110dd2223ad4fd88b9c175c735fb099
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-04-22 10:02:50 -0400

    Adding Icon in Action Bar

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -155,6 +155,7 @@ public class TimelineActivity extends AppCompatActivity {
              public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
                  Log.e(TAG, "onFailure!", throwable);
              }
+
          });
     }
 }
